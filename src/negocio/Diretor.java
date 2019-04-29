9a89d4aa49a5135dcb496d8be139a9647c33f67e
Mariana Soares <marianas20101@gmail.com>
2019-04-29 09:01:04 -0300

    Initial commit

---
+++ src/negocio/Diretor.java
@@ -0,0 +1,30 @@
+package negocio;
+
+public class Diretor extends Funcionario{
+
+	public float adicional;
+	
+	@Override
+	public void exibir() {
+		System.out.println(this.toString());
+	}
+	
+	@Override
+	public String toString() {
+		return String.format(
+				"%s: %s\n" +
+				"Adicional: %.2f\n",
+				"M".equals(this.getSexo()) ? "Diretor" : "Diretora",
+				super.toString(),jk
+				this.getAdicional()
+			);
+	}
+	
+	public float getAdicional() {
+		return adicional;
+	}
+
+	public void setAdicional(float adicional) {
+		this.adicional = adicional;
+	}
+}
---
+++ src/testes/TestaDiretor.java
@@ -0,0 +1,21 @@
+package testes;
+
+import negocio.Diretor;
+
+public class TestaDiretor {
+
+	public static void main(String[] args) {
+		Diretor diretor = new Diretor();
+		
+		diretor.setNome("tio manoel");
+		diretor.setLogin("tio.manoel");
+		diretor.setSenha("123456");
+		diretor.setSexo("M");
+		diretor.setUniversidade("Infnet");
+		diretor.setSalario(1000);
+		diretor.setAdicional(500);
+		
+		diretor.exibir();
+	}
+
+}
